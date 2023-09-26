50848e16d7466474cfacf2721bb4cd9904c33634
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-26 12:46:55 +0200

    Add new article to places section

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -26,26 +26,26 @@ $(document).ready(function () {
       },
       success: function (data) {
           for (place in data) {
-          $('article').html(
-            `<article>
-              <div class="title_box">
-                <h2>${place.name}</h2>
-                  <div class="price_by_night">$${place.price_by_night}</div>
+          const article = $('<article>');
+          article.html(
+            `<div class="title_box">
+              <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">
+                ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}
               </div>
-              <div class="information">
-                <div class="max_guest">
-                  ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}
-                </div>
-                <div class="number_rooms">
-                  ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}
-                </div>
-                <div class="number_bathrooms">
-                  ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}
-                </div>
+              <div class="number_rooms">
+                ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}
               </div>
-              <div class="description">${place.description}</div>
-            </article>`
+              <div class="number_bathrooms">
+                ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}
+              </div>
+            </div>
+            <div class="description">${place.description}</div>`
           );
+          $('section.places').append(article);
         }
       }
     });
